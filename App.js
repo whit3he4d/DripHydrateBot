87e77dddfb1b7c6b2380af59e9c7fa168b3c6caf
whit3he4d <whit3@gmx.com>
2022-03-11 16:26:56 +0100

    working with background image

--- App.js
+++ App.js
@@ -1,55 +1,26 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import {connect} from 'react-redux';
-import Heartbeat from './Heartbeat';
-import heart from './heart.png';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import background from './background.jpg';
+
+// import Heartbeat from './Heartbeat';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-  },
-  view: {
-    flex: 0.5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: 'gray',
-    padding: 10,
-    margin: 10,
-  },
-  text: {
-    fontSize: 20,
-    color: 'white',
   },
 });
 
-const App = ({heartBeat}) => {
-  const imageSize = heartBeat ? 150 : 100;
+const App = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.view}>
-        <Image
-          source={heart}
-          style={{width: imageSize, height: imageSize}}
-          resizeMode="contain"
-        />
-      </View>
-      <View style={styles.view}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => Heartbeat.startService()}>
-          <Text style={styles.instructions}>Start</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => Heartbeat.stopService()}>
-          <Text style={styles.instructions}>Stop</Text>
-        </TouchableOpacity>
-      </View>
+      <ImageBackground source={background} resizeMode="cover" style={styles.image}>
+
+      </ImageBackground>
     </View>
   );
 };
